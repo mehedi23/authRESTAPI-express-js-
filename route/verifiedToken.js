6d5c164dee8d7759f9a30e1c71e9022241484f71
Mehedi Hassan Satu <63834645+mehedi23@users.noreply.github.com>
2021-09-12 08:17:13 +0600

    Update verifiedToken.js

--- route/verifiedToken.js
+++ route/verifiedToken.js
@@ -8,11 +8,13 @@ function authVeryfi( req , res , next ){
     try {
         const verified = jwt.verify(token , process.env.TOKEN_SECRET);
         req.user = verified;
+        
+        next()
     } catch (error) {
         res.status(400).send('Invalid Token')
     }
 
-    next()
+    
 }
 
 module.exports = authVeryfi
